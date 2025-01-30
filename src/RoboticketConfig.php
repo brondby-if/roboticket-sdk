a6804b71c271480cc360b79070375ce3ab8a826a
carnevalle <54747+carnevalle@users.noreply.github.com>
2025-01-30 10:48:02 +0000

    Fix styling

--- src/RoboticketConfig.php
+++ src/RoboticketConfig.php
@@ -4,20 +4,20 @@ namespace Brondby\Roboticket;
 
 use Exception;
 
-class RoboticketConfig{
+class RoboticketConfig
+{
     public function __construct(
         public readonly string $key,
         public readonly string $client_id,
         public readonly string $base_url
-    )
-    {}
+    ) {}
 
     public static function fromArray(array $config): RoboticketConfig
     {
-        if(
-            !array_key_exists('key', $config) ||
-            !array_key_exists('client_id', $config) ||
-            !array_key_exists('base_url', $config)
+        if (
+            ! array_key_exists('key', $config) ||
+            ! array_key_exists('client_id', $config) ||
+            ! array_key_exists('base_url', $config)
         ) {
             throw new Exception('Roboticket requires key, client_id and base_url');
         }
